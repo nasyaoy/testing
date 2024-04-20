28c74b66d56b45b8c2f1d6b177ff65d69db3ba1f
nasyaoy <122662286+nasyaoy@users.noreply.github.com>
2024-04-20 14:47:56 +0500

    доработанный тест

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Ьассив с с вопросами, вариантами ответов и правильными ответами
+// Массив с с вопросами, вариантами ответов и правильными ответами
 let questions = [
     {
         question: "Откуда эта строчка: 'You really know how to make me cry'",
@@ -13,7 +13,22 @@ let questions = [
     {
         question: "Откуда эта строчка: 'You call me again, drunk in your Benz'",
         options: ["Happier Than Ever", "ilomilo", "TV", "watch"],
-        correctAnswer: "TV"
+        correctAnswer: "Happier Than Ever"
+    },
+    {
+        question: "Откуда эта строчка: 'So, where did you go?I should know, but it's cold'",
+        options: ["NDA", "ilomilo", "bellyache", "Oxitocin"],
+        correctAnswer: "ilomilo"
+    },
+    {
+        question: "Откуда эта строчка: ''Cause I loved you then and I love you now'",
+        options: ["Bored", "Your Power", "Oxitocin", "Male Fantasy"],
+        correctAnswer: "Male Fantasy"
+    },
+    {
+        question: "Откуда эта строчка: 'Just can't get enough guy'",
+        options: ["Hotline", "Bored", "No Time To Die", "bad guy"],
+        correctAnswer: "bad guy"
     }
 ]
 let currentQuestion = 0 //текущий вопрос
@@ -35,8 +50,52 @@ function displayQuestion() {
         let button = document.createElement('button');
         optionsElement.append(button);
         button.textContent = option;
-        button.classList.add('btn')
+        button.classList.add('btn');
     });
+
+    // добавить обработчик события на блок с кнопками
+    optionsElement.addEventListener('click', (e) => {
+        // записать переменную элемент на который кликнули
+        let target = e.target;
+        // вызвать функцию перехода к следующему вопросу и передать ей текстовое содержимое кнопки по которой кликнули
+        nextQuestion(target.textContent);
+    }, { once: true });
 }
+// функция перехода к следующему вопросу
+function nextQuestion(answer) {
+    // если кликнули на правитьный ответ то 
+    if (answer === questions[currentQuestion].correctAnswer) {
+        // увеличиваем на 1 счетчик правильных ответов
+        correctAnswers++;
+    }
+    currentQuestion++; // перейти к следующему вопросу 
+    if (currentQuestion < questions.length) {
+        displayQuestion(); // отобразить следующий вопрос
+    } else {
+        displayResult();
+    }
+};
+
+// функция отображения результатов теста
+function displayResult() {
+    let questionElement = document.getElementById('question'); // получить блок с вопросом
+    let optionsElement = document.getElementById('options'); // получить блок с кнопками
+    let resultElement = document.getElementById('result'); // получить блок дл яотображения результата
+    questionElement.style.display = 'none'; // выключть видимость блока вопросов
+    optionsElement.style.display = 'none'; // выключть видимость блока ответов
+    resultElement.textContent = `правильных ответов ${correctAnswers} из ${questions.length}`;// отобразить результат теста
+}
+
+let startBtn = document.querySelector('.start__btn');
+startBtn.addEventListener('click', (e) => {
+    e.preventDefault
+    let form = document.querySelector('#nameInput');
+    form.style.display = 'none';
+    let questionElement = document.getElementById('question');
+    let optionsElement = document.getElementById('options');
+    questionElement.style.visibility = 'visible';
+    optionsElement.style.visibility = 'visible';
+});
+
 
 displayQuestion();
